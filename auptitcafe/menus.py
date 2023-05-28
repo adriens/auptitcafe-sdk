0f663cbc30b10ed60f9c97f40ae86aace988e7e2
SALES <Adrien.Sales@gmail.com>
2023-05-28 13:43:02 +1100

    Update menus.py

--- auptitcafe/menus.py
+++ auptitcafe/menus.py
@@ -16,6 +16,10 @@ class Menus:
         soup = BeautifulSoup(response.text, 'html.parser')
         title = soup.find('h2', class_='elementor-heading-title elementor-size-default')
         out = title.text.strip()
+        # remove special characters
+        caracteres_speciaux = "~!@#$%^&*()_+{}:\"<>?|\\-=[];',./"
+        for caractere in caracteres_speciaux:
+            out = out.replace(caractere, "")
         return out
 
 
